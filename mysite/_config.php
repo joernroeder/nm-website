79678d2ff9c9506d06123e8f76866aa8d5e0bea9
Jonathan Pirnay <mail@johnnycrab.com>
2013-01-13 16:19:37 +0100

    added silverstripe 3 framework

---
+++ mysite/_config.php
@@ -0,0 +1,26 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'wp185.webpack.hosteurope.de',
+	"username" => 'db10674345-nmer',
+	"password" => 'y83WIsZk1SIb',
+	"database" => 'db10674345-nm',
+	"path" => '',
+);
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('tutorial');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
---
+++ mysite/code/RootURLController.php
@@ -0,0 +1,9 @@
+<?php
+
+class RootURLController extends Controller {
+
+	function index() {
+		echo "<html>Your site is now set up. Start adding controllers to mysite to get started.</html>";
+	}
+
+}
