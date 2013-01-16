af873f5982eef0e6d342a8a89bd51ff4d261bcb9
Jonathan Pirnay <mail@johnnycrab.com>
2013-01-16 14:28:08 +0100

    db config for phpunit testing + added _ss_environment

--- nm/_config.php
+++ nm/_config.php
@@ -1,17 +1,13 @@
 <?php
 
+
+ini_set('memory_limit', '1000M');
+
 global $project;
 $project = 'nm';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'wp185.webpack.hosteurope.de',
-	"username" => 'db10674345-nmer',
-	"password" => 'y83WIsZk1SIb',
-	"database" => 'db10674345-nm',
-	"path" => '',
-);
+global $database;
+$database = 'db10674345-nm';
 
 MySQLDatabase::set_connection_charset('utf8');
 
@@ -25,4 +21,26 @@ i18n::set_locale('en_US');
 // Enable nested URLs for this site (e.g. page/sub-page/)
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
-Director::set_environment_type('dev');
+require_once('conf/ConfigureFromEnv.php');
+
+// Database config for php unit testing
+if (Director::isDev()) {
+	Session::start();
+	if(@$_GET['db']) {
+		$db = $_GET['db'];
+	} elseif(@$_SESSION['db']) {
+		$db = $_SESSION['db'];
+	} else {
+		$db = null;
+	}
+
+	if($db) {
+		global $databaseConfig;
+		if($db == 'test') {
+			$databaseConfig['type'] = 'MySQLDatabase';
+			$databaseConfig['server'] = TEST_DB_SERVER;
+			$databaseConfig['username'] = TEST_DB_USER;
+			$databaseConfig['password'] = TEST_DB_PASS;
+		} 
+	}
+}
