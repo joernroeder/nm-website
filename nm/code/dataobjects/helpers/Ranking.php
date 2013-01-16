0b87edbb9d74c95045c27fc3dd56b8729cfe8392
Jörn Röder <kontakt@joernroeder.de>
2013-01-16 12:38:42 +0100

    Added Ranking

---
+++ nm/code/dataobjects/helpers/Ranking.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ *    000000  0000000000    000000000000000000
+ *    000000  0000000000    00000000000000000000
+ *    000000      000000    000000   00000  000000
+ *    000000      000000    000000     000  000000
+ *     00000      000000    000000       0  000000
+ *       000      000000    000000          000000
+ *         0      000000    000000          000000
+ *
+ *    Neue Medien - Kunsthochschule Kassel
+ *    http://neuemedienkassel.de
+ *
+ */
+
+/**
+ * 
+ * {@link Person}
+ */
+class Ranking extends DataObject {
+
+	static $db = array(
+		'Ranking'	=> 'Integer'	// Sortierung: Größer -> Wichtiger
+	);
+
+}
+
