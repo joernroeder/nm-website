3d4c80fb889f2d7038c4992e449fd6ed5b8efebd
Jörn Röder <kontakt@joernroeder.de>
2013-01-21 17:08:10 +0100

    fixed Teaser-Field

--- nm/code/extensions/TeaserCMSFieldsExtension.php
+++ nm/code/extensions/TeaserCMSFieldsExtension.php
@@ -3,9 +3,8 @@
 class TeaserCMSFieldsExtension extends DataExtension {
 
 	public function updateCMSFields(FieldList $fields) {
-		$teaserTextArea = new TextareaField('TeaserText', 'Teaser');
-		$fields->removeFieldFromTab('Root.Main', 'TeaserText');
-		$fields->addFieldToTab('Root.Main', $teaserTextArea, 'StartDate');
-	}	
+		$fields->removeByName('TeaserText');
+		$fields->insertBefore(new TextareaField('TeaserText', 'Teaser'), 'Text');
+	}
 
 }
