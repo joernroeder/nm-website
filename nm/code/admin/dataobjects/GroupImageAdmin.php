81eaf937f5dd7d7c8e8b3f624c4e8a8a0d82fc90
Jörn Röder <kontakt@joernroeder.de>
2013-01-21 16:12:19 +0100

    added GroupImageAdmin

---
+++ nm/code/admin/dataobjects/GroupImageAdmin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ *    000000  0000000000    000000000000000000
+ *    000000  0000000000    00000000000000000000
+ *    000000      000000    000000   00000  000000
+ *    000000      000000    000000     000  000000
+ *     00000      000000    000000       0  000000
+ *       000      000000    000000          000000
+ *         0      000000    000000          000000
+ *
+ *    Neue Medien - Kunsthochschule Kassel
+ *    http://neuemedienkassel.de
+ *
+ */
+class GroupImageAdmin extends ModelAdmin {
+
+	public static $managed_models = array(
+		'GroupImage'
+	);
+
+	static $url_segment = 'group-images';
+	static $menu_title = 'Group-Images';
+
+	static $extensions = array(
+		'IconizedModelAdminExtension'
+	);
+
+}
