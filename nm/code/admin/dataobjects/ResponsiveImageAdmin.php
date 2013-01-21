39f44fc97df055243e7748f7e44032dab1e922dd
Jörn Röder <kontakt@joernroeder.de>
2013-01-21 16:06:03 +0100

    added ResponsiveImageAdmin

---
+++ nm/code/admin/dataobjects/ResponsiveImageAdmin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ *    000000  0000000000    000000000000000000
+ *    000000  0000000000    00000000000000000000
+ *    000000      000000    000000   00000  000000
+ *    000000      000000    000000     000  000000
+ *     00000      000000    000000       0  000000
+ *       000      000000    000000          000000
+ *         0      000000    000000          000000
+ *
+ *    Neue Medien - Kunsthochschule Kassel
+ *    http://neuemedienkassel.de
+ *
+ */
+class ResponsiveImageAdmin extends ModelAdmin {
+
+	public static $managed_models = array(
+		'ResponsiveImage'
+	);
+
+	static $url_segment = 'images';
+	static $menu_title = 'Images';
+
+	static $extensions = array(
+		'IconizedModelAdminExtension'
+	);
+
+}
+
