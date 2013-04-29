c128a7b6d57e427a517dc4bfc910bc4a0ace6d19
Jonathan Pirnay <mail@johnnycrab.com>
2013-04-29 19:13:35 +0200

    removed theme from php config

--- nm/_config.php
+++ nm/_config.php
@@ -17,8 +17,6 @@ $database = 'db10674345-nm';
 
 MySQLDatabase::set_connection_charset('utf8');
 
-SSViewer::set_theme('nm');
-
 i18n::set_locale('en_US');
 if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
@@ -39,28 +37,3 @@ SS_Cache::pick_backend('two-level', 'any', 20);*/
 
 // include environment config 
 require_once('conf/ConfigureFromEnv.php');
-
-
-
-// ! Database config for php unit testing ---------
-
-if (Director::isDev()) {
-	Session::start();
-	if(@$_GET['db']) {
-		$db = $_GET['db'];
-	} elseif(@$_SESSION['db']) {
-		$db = $_SESSION['db'];
-	} else {
-		$db = null;
-	}
-
-	if($db) {
-		global $databaseConfig;
-		if($db == 'test') {
-			$databaseConfig['type'] = 'MySQLDatabase';
-			$databaseConfig['server'] = TEST_DB_SERVER;
-			$databaseConfig['username'] = TEST_DB_USER;
-			$databaseConfig['password'] = TEST_DB_PASS;
-		} 
-	}
-}
