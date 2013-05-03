d6252da49f73434d45b875ced0509a36a155539a
Jörn Röder <kontakt@joernroeder.de>
2013-05-03 10:40:13 +0200

    removed test prints

--- nm/code/dataobjects/helpers/SubdomainFile.php
+++ nm/code/dataobjects/helpers/SubdomainFile.php
@@ -3,8 +3,6 @@
 class SubdomainFile extends File {
 
 	function Link() {
-		print_r("custom link");
-
 		return parent::Link();
 	}
 
--- nm/code/dataobjects/helpers/SubdomainResponsiveImageObject.php
+++ nm/code/dataobjects/helpers/SubdomainResponsiveImageObject.php
@@ -3,8 +3,6 @@
 class SubdomainResponsiveImageObject extends ResponsiveImageObject {
 
 	function Link() {
-		print_r("custom image link");
-
 		return parent::Link();
 	}
 
