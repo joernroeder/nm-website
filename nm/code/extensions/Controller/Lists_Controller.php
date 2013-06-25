22daa443dbc5334672eb8ceb62cb5bccd656e15c
Jonathan Pirnay <mail@johnnycrab.com>
2013-06-25 23:13:37 +0200

    Lists Controller (d-o-n-e)

--- nm/code/extensions/Controller/Lists_Controller.php
+++ nm/code/extensions/Controller/Lists_Controller.php
@@ -14,6 +14,16 @@ class Lists_Controller extends Controller {
 		if (Member::CurrentUserID()) {
 			$this->currentUser = Member::CurrentUser();
 		}
+
+		$this->addContentTypeHeader();
+	}
+
+	public function addContentTypeHeader() {
+		$contentType = 'application/json; charset="utf-8"';
+		$response = $this->getResponse();
+
+		$response->removeHeader('Content-Type');
+		$response->addHeader('Content-Type', $contentType);
 	}
 
 	/**
@@ -27,10 +37,45 @@ class Lists_Controller extends Controller {
 	 * 	
 	 */
 	public function all() {
-		if (!$this->currentUser) return json_encode(array());
+		$out = array();
 
+		if (!$this->currentUser) return json_encode($out);
 		
+		foreach (array('Project', 'Exhibition', 'Excursion', 'Workshop') as $type) {
+			$out[$type] = $this->getObjectsAsList($type, 'Title', '', 'Title');
+		}
 
+		$out['Category'] = $this->getObjectsAsList('Category', 'Title', '', 'Title');
+
+		$out['Person'] = $this->getObjectsAsList('Person', 'FullName', '', 'Surname');
+
+		return json_encode($out);
+
+	}
+
+	/**
+	 * @todo JJ_Aggregate and Cache
+	 */
+	private function getObjectsAsList($className, $att, $where = '', $sortBy = null, $sortOrder = 'ASC') {
+		$objs = DataList::create($className)->where($where);
+		if ($sortBy) $objs->sort($sortBy, $sortOrder);
+
+		$out = array();
+		
+		foreach ($objs as $obj) {
+			
+			$theAtt = $obj->hasMethod($att) ? $obj->$att() : $obj->$att;
+			
+			if ($theAtt) {
+				$out[] = array(
+					'ID'	=> $obj->ID,
+					$att 	=> $theAtt
+				);	
+			}
+			
+		}
+
+		return $out;
 	}
 
 }
