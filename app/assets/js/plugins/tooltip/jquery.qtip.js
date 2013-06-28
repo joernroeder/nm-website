566506c8a8ed4cff3c87d1513944ea27e8ed54e0
Jonathan Pirnay <mail@johnnycrab.com>
2013-06-28 22:40:29 +0200

    removed shite AMD implementation

--- app/assets/js/plugins/tooltip/jquery.qtip.js
+++ app/assets/js/plugins/tooltip/jquery.qtip.js
@@ -12,19 +12,9 @@
  */
 /*global window: false, jQuery: false, console: false, define: false */
 
-/* Cache window, document, undefined */
-(function( window, document, undefined ) {
 
 // Uses AMD or browser globals to create a jQuery plugin.
-(function( factory ) {
-	"use strict";
-	if(typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
-	}
-	else if(jQuery && !jQuery.fn.qtip) {
-		factory(jQuery);
-	}
-}
+
 (function($) {
 	/* This currently causes issues with Safari 6, so for it's disabled */
 	//"use strict"; // (Dis)able ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
@@ -2035,8 +2025,8 @@ QTIP.defaults = {
 	}
 
 	return adjusted;
-};;}));
-}( window, document ));
+};;})(jQuery);
+
 
 
 
