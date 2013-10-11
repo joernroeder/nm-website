075cc56e608f2490c11851dd9ea2dce7852e6724
Jörn Röder <kontakt@joernroeder.de>
2013-10-11 14:15:56 +0200

    testing

--- app/.git-hooks/pre-commit.js
+++ app/.git-hooks/pre-commit.js
@@ -1,24 +1,38 @@
 var exec = require('child_process').exec;
 
-console.log(__dirname);
-exec('cd ' + __dirname + '/../../; git diff --cached --quiet', function (err, stdout, stderr) {
+exec('cd ' + __dirname + '/../../app; git diff --cached --quiet', function (err, stdout, stderr) {
+
+	exec('cd app; ls', function (err, out, e) {
+		console.log(err);
+		console.log(out);
+	});
 
 	// only run if there are staged changes
 	// i.e. what you would be committing if you ran "git commit" without "-a" option.
 	if (err) {
 		// go to the app folder, kick off grunt and come back
-		exec('cd app; grunt release; cd ..', function (err, stdout, stderr) {
+		exec('cd ./app', function (err, stdout, stderr) {
 			
 			if (err) {
 				console.log(stderr);
 				process.exit(-1);
 			}
-			// append dist to the commit
-			else {
-				exec('git add ./app/dist/*', function () {
-					process.exit(0);
-				});
-			}
+
+			exec('grunt release', function (err, stdout, stderr) {
+				console.log('grunt callback');
+				process.exit(-1);
+
+				if (err) {
+					console.log(stderr);
+					process.exit(-1);
+				}
+				// append dist to the commit
+				else {
+					exec('git add ./app/dist/*', function () {
+						process.exit(0);
+					});
+				}
+			});
 		});
 	}
 });
