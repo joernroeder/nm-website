d456ced94ab3b42dc96a2b71977625a9548f6354
Jonathan Pirnay <mail@johnnycrab.com>
2013-10-11 11:38:38 +0200

    test

--- app/app/modules/Portfolio.js
+++ app/app/modules/Portfolio.js
@@ -8,6 +8,7 @@ define(['app', 'modules/JJPackery'], function(app, JJPackery) {
   };
   Portfolio.Views.PackeryContainer = JJPackery.Views.Container.extend({
     cleanup: function() {
+      console.log('cleanup');
       return Backbone.Events.off('search', this.handleSearch);
     },
     initialize: function() {
